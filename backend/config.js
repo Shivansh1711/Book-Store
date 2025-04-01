286ad3aba3dfc5b6e972406110677cffabbce188
Shivansh ojha <119355396+Shivansh1711@users.noreply.github.com>
2025-04-02 00:28:19 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
-export const MONGODB_URI="mongodb+srv://shivansh1711:Rishu%40123@cluster0.hrl0nyx.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODB_URI="Your URL";
 // export const MONGODB_URI="mongodb://localhost:27017/bookstore";
